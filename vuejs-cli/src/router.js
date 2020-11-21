4ac05869fa29e5924506b74808d993239d731bca
Richard Willian <richardwillian.2008@hotmail.com>
2020-11-21 18:51:40 -0300

    Faz melhorias de estilo na tela de 'Painel de Leads'

--- vuejs-cli/src/router.js
+++ vuejs-cli/src/router.js
@@ -1,22 +1,15 @@
 import Vue from "vue"
 import Router from "vue-router"
 import Home from "../src/views/Home"
-import FormCreateAccount from "../src/components/FormCreateAccount/FormCreateAccount"
 
 
 Vue.use(Router)
 
 const routes = [{
-        name: "home",
-        path: "/",
-        component: Home
-    },
-    {
-        name: "formularioCriacaoConta",
-        path: "/formularioCriacaoConta",
-        component: FormCreateAccount
-    }
-]
+    name: "home",
+    path: "/",
+    component: Home
+}, ]
 
 const router = new Router({ routes });
 
