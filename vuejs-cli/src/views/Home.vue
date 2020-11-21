da9e6837f3afbfcdc2661d3b3c28857085c7a0c6
Richard Willian <richardwillian.2008@hotmail.com>
2020-11-21 18:08:17 -0300

    Faz melhorias estéticas na interface da tela de 'painel de leads'

--- vuejs-cli/src/views/Home.vue
+++ vuejs-cli/src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <div>
     <NavBar />
-    <PainelLeads />
+    <div class="mb-2 mt-2">
+      <PainelLeads />
+    </div>
+    <Footer />
   </div>
 </template>
 
 <script>
 import NavBar from "@/components/NavBar";
+import Footer from "@/components/FooterBar";
 import PainelLeads from "@/components/PainelLeads/PainelLeads";
 
 export default {
   name: "App",
   components: {
     NavBar,
+    Footer,
     PainelLeads,
   },
 };
